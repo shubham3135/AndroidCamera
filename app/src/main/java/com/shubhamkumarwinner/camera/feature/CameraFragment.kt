e60f57c6e906d37f6b9f0f5e0058af2a6347eea6
Shubham <shubhamkumarwinner@gmail.com>
2021-08-25 23:41:59 +0530

    record and play video

--- app/src/main/java/com/shubhamkumarwinner/camera/feature/CameraFragment.kt
+++ app/src/main/java/com/shubhamkumarwinner/camera/feature/CameraFragment.kt
@@ -1,14 +1,20 @@
 package com.shubhamkumarwinner.camera.feature
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.getBitmap
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.shubhamkumarwinner.camera.databinding.CameraFragmentBinding
 import java.io.ByteArrayOutputStream
@@ -17,6 +23,7 @@ import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
+const val REQUEST_VIDEO_CAPTURE = 1
 class CameraFragment : Fragment() {
 //    private val viewModel: CameraViewModel by viewModels()
     lateinit var currentPhotoPath: String
@@ -25,8 +32,8 @@ class CameraFragment : Fragment() {
     private val takePicture = registerForActivityResult(
         ActivityResultContracts
         .TakePicturePreview()) {
-        binding.imageView.setImageBitmap(it)
-        saveToFile(it)
+//        binding.imageView.setImageBitmap(it)
+        saveImageToFile(it)
     }
 
 
@@ -37,19 +44,39 @@ class CameraFragment : Fragment() {
     ): View? {
         binding = CameraFragmentBinding.inflate(layoutInflater, container, false)
         binding.openCamera.setOnClickListener {
-            takePicture.launch(null)
+//            takePicture.launch(null)
+//            takeVideo.launch(null)
+            dispatchTakeVideoIntent()
+
         }
         return binding.root
     }
 
-    private fun saveToFile(bitmap: Bitmap): File? {
+    private fun dispatchTakeVideoIntent() {
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            takeVideoIntent.resolveActivity(requireActivity().packageManager)?.also {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            val videoUri: Uri = data?.data!!
+//            saveVideoToFile(videoUri)
+            binding.videoView.setVideoURI(videoUri)
+            binding.videoView.start()
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun saveImageToFile(bitmap: Bitmap): File? {
         var file: File? = null
         return try {
             val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
             file = File("/storage/emulated/0/Pictures/${"JPEG_${timeStamp}_.jpg"}")
             file.createNewFile()
-
-            Log.d("test", "name: $file")
 
             //Convert bitmap to byte array
             val bos = ByteArrayOutputStream()
@@ -67,6 +94,31 @@ class CameraFragment : Fragment() {
             file // it will return null
         }
     }
+
+    /*private fun saveVideoToFile(uri: Uri): File? {
+        var file: File? = null
+        var bitmap = getBitmap(requireActivity().contentResolver, uri)
+        return try {
+            val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            file = File("/storage/emulated/0/Pictures/${"VID_${timeStamp}_.mp4"}")
+            file.createNewFile()
+
+            //Convert bitmap to byte array
+            val bos = ByteArrayOutputStream()
+//            bitmap.compress(Bitmap.CompressFormat.WEBP, 100, bos) // YOU can also save it in JPEG
+            val bitmapData = bos.toByteArray()
+
+            //write the bytes in file
+            val fos = FileOutputStream(file)
+            fos.write(bitmapData)
+            fos.flush()
+            fos.close()
+            file
+        } catch (e: Exception) {
+            e.printStackTrace()
+            file // it will return null
+        }
+    }*/
 
 }
 
